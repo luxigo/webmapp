0a7b7bd685c4cafc38b0b0a244cd0df9545eec76
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2016-05-08 09:09:56 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trackerApp
+ */
+angular.module('trackerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
